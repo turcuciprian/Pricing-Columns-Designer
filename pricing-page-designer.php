d6644718931bc592c306cf25e2c95b3d9d97295b
Ciprian Turcu <turcuciprian1@gmail.com>
2017-02-13 08:53:03 +0200

    main file added with basic plugin text

---
+++ pricing-page-designer.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: Pricing Page Designer
+  Plugin URI: http://ciprianturcu.com
+  Description: A plugin that adds pricing columns and rows, easily to any page or post.
+  Version: 1.0
+  Author: turcuciprian
+  Author URI: http://ciprianturcu.com
+  License: GPLv2 or later
+  Text Domain: ppd
+ */
