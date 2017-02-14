272fa79a06dd92b58745084ff9a22305eb3b3d06
Ciprian Turcu <turcuciprian1@gmail.com>
2017-02-14 19:04:05 +0200

    added custom script file with default code, enqueued angular and custom script

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -2,6 +2,7 @@
 
 add_action('admin_enqueue_scripts', 'ppdEnqueueAdmin');
 function ppdEnqueueAdmin(){
-  // wp_enqueue_style('rta-jquery-ui', plugin_dir_url(__FILE__) . 'jquery-ui.min.css');
+  wp_enqueue_style('ppd-angular', plugin_dir_url(__FILE__) . '../js/angular.min.js');
+  wp_enqueue_style('rta-jquery-ui', plugin_dir_url(__FILE__) . '../js/script.js',array('ppd-angular'));
   wp_enqueue_style('ppd', plugin_dir_url(__FILE__) . '../style.css');
 }
