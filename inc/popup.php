06a7dfb411fa83bef7c64f18a89201f140721514
Ciprian Turcu <turcuciprian1@gmail.com>
2017-02-19 23:09:43 +0200

    added highlight functionality

--- inc/popup.php
+++ inc/popup.php
@@ -17,13 +17,16 @@ function ppdPopupHTML()
           </div>
           <div class="row" ng-repeat="(sideKey,sideValue) in sideColumn track by $index">
             {{sideValue}}
+            <div class="delete" ng-mouseover="changeRowIndex(sideKey)" ng-mouseleave="rowIndex = -1">
+Delete
+            </div>
           </div>
         </div>
         <div class="column" ng-repeat="(key,value) in grid track by $index">
           <div class="row title side">
              Title {{key+1}}
           </div>
-          <div class="row" idVal="{{key}}" ng-repeat="(key2,value2) in value track by $index">
+          <div class="row" idVal="{{key}}" ng-class="{highlight: highlughtIfIndex(key2)}" ng-repeat="(key2,value2) in value track by $index">
             {{value2}}
           </div>
           <div ng-click="removeColumn($index)" ng-show="grid.length > 1" class="delete">
