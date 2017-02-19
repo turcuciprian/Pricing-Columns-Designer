a218552f1b71615cd053163dac0261a9bb5a68b9
Ciprian Turcu <turcuciprian1@gmail.com>
2017-02-19 23:10:06 +0200

    added highlight functionality

---
+++ src/javascript/custom/script.js
@@ -0,0 +1,55 @@
+//the angular part
+var ppdMod = angular.module('ppdAng', []);
+ppdMod.controller('ppdControler', ['$scope', function($scope) {
+
+    $scope.grid = [
+        ['Empty']
+    ];
+    $scope.sideColumn = ['Empty'];
+    $scope.rowIndex = -1;
+
+    $scope.addRow = function() {
+        var newGenArr = $scope.grid;
+        newGenArr.forEach(function(value, index) {
+            value.push('Empty');
+        });
+        $scope.sideColumn.push($scope.sideColumn[0]);
+
+        $scope.grid = newGenArr;
+    };
+    $scope.addColumn = function() {
+        var newGenArr = $scope.grid;
+        if (newGenArr[0]) {
+            newGenArr.push(returnArrLen(newGenArr[0].length));
+        } else {
+            newGenArr.push(['Empty']);
+        }
+    };
+    $scope.changeRowIndex = function(ind) {
+        $scope.rowIndex = ind;
+    }
+    $scope.highlughtIfIndex = function(ind) {
+        if (ind === $scope.rowIndex) {
+            return true;
+        }
+        return false;
+
+
+    };
+    $scope.removeColumn = function(index) {
+        $scope.grid.splice(index, 1);
+    };
+    var returnArrLen = function(size) {
+        var arr = [];
+        while (arr.length < size) {
+            arr.push('Empty');
+        }
+        return arr;
+    };
+}]);
+//the jquery part
+jQuery(document).ready(function($) {
+
+
+
+});
