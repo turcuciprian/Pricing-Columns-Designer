36d01d2168606cd3397cc8d4820453838b149bed
Ciprian Turcu <turcuciprian1@gmail.com>
2017-03-07 17:43:30 +0200

    added editable text functionality to side cells

--- inc/popup.php
+++ inc/popup.php
@@ -17,6 +17,7 @@ function ppdPopupHTML()
           </div>
           <div class="row cell" ng-class="{highlight: highlughtIfIndex(sideKey)}" ng-repeat="(sideKey,sideValue) in sideColumn track by $index">
             {{sideValue}}
+            <input ng-model="sideColumn[sideKey]" type="text" class="editable" name="" value="{{sideColumn[sideKey]}}">
             <div class="delete" ng-show="grid[0].length > 1" ng-mouseleave="changeRowIndex(-1)" ng-mouseover="changeRowIndex(sideKey)" ng-click="deleteRow(sideKey)">
 Delete
             </div>
